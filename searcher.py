79b8b8fb951470ea4236633c742f8c1b6be6670d
Gabriel Kramer-Garcia <glk2110@columbia.edu>
2017-11-06 22:10:32 -0500

    wef

--- searcher.py
+++ searcher.py
@@ -45,7 +45,7 @@ def makeQuery(apiKey, engineID, relation, threshold, query, k):
 			cx=engineID,
 		).execute()
 		print("=========== Iteration: " + str(iterationNum) + " - Query: " + query + " ===========")
-		for i in range(1):
+		for i in range(2):
 			solution = res[u'items'][i][u'link'].encode('ascii','ignore')
 			print("Processing: " + solution)
 			r = urllib.urlopen(solution).read()
