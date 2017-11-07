48cad0558d1887b804560520f6d0a1ee974ca520
Gabriel Kramer-Garcia <glk2110@columbia.edu>
2017-11-07 14:49:54 -0500

    wef

--- searcher.py
+++ searcher.py
@@ -57,7 +57,8 @@ def makeQuery(apiKey, engineID, relation, threshold, query, k):
 				continue
 			soup = BeautifulSoup(r)
 			texts = soup.find_all('p')[0:5]
-			print(texts.strip())
+			for text in texts:
+				print(text.strip())
 			client = NLPCoreClient('stanford-corenlp-full-2017-06-09')
 			properties1 = {
 				"annotators": "tokenize,ssplit,pos,lemma,ner", #Second pipeline; leave out parse,relation for first
