7c980d709b7f9bf6d62252bdf266fd88b1f9a0ab
Gabriel Kramer-Garcia <glk2110@columbia.edu>
2017-11-07 23:16:08 -0500

    wef

--- searcher.py
+++ searcher.py
@@ -113,7 +113,12 @@ def makeQuery(apiKey, engineID, relation, threshold, query, k):
 		goodTuples = len(tuples)
 		print("Number of tuples after pruning: " + str(goodTuples))
 		print("================== ALL RELATIONS =================")
-		for tup in tuples:
+		myTuples = list(tuples)
+		myTuples.sort(key=operator.itemgetter(1))
+		count = =
+		for tup in myTuples:
+			if count == 0:
+				query = enVa2 + " " + enVa1
 			print("RelationType: " + relationName + "  | Confidence: " + confidence + "		| Entity #1= " + enVa1 + " (" + enTy1 + ")	| Entity #2: " + enVa2 + " (" + enTy2 + ")")
 		iterationNum += 1
 
