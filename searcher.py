947ce43f9380d3a574e448a8bd2fb31e10e7edfc
Gabriel Kramer-Garcia <glk2110@columbia.edu>
2017-11-08 11:15:15 -0500

    wef

--- searcher.py
+++ searcher.py
@@ -75,7 +75,7 @@ def makeQuery(apiKey, engineID, relation, threshold, query, k):
 				"ner.useSUTime": "0"
 				}
 			doc = client.annotate(text=result, properties=properties)
-			print(doc.sentences[0].tokens)
+			print(doc.sentences[0].tokens.ner)
 			newsentence = ""
 			rSentences = []
 			finalSentences = []
