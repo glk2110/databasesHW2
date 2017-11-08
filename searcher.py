98d2340ad92b31da29db603e3b28e0c2720dfd8e
Gabriel Kramer-Garcia <glk2110@columbia.edu>
2017-11-07 22:12:53 -0500

    wef

--- searcher.py
+++ searcher.py
@@ -91,13 +91,12 @@ def makeQuery(apiKey, engineID, relation, threshold, query, k):
 			for s3 in list1:
 				for s4 in s3.relations:
 					print("=============== EXTRACTED RELATION ===============")
-					print(s4)
 					newsentence1 = ""
 					for x1 in s3.tokens:
 						newsentence1 += " " + x1.word
 					print("Sentence: " + newsentence1)
 					confidence = s4.probabilities[relationName]
-					enTy1 = ""
+					enTy1 = s4.entities
 					enVa1 = ""
 					enTy2 = ""
 					enVa2 = ""
